6e71f5bde44f90b6272c93d59bfc17a19f720184
naoto4420-pixel <naoto4420@gmail.com>
2026-02-25 15:25:16 +0900

    練習１（プリミティブ型）

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const productName = "ノートパソコン";
+const price = 55000;
+const isOnSale = false;
+console.log(`商品名：${productName}`);
+console.log(`価格：${price}`);
+console.log(`セール中：${isOnSale}`);
+let stockCount = 2;
+console.log(`在庫数：${stockCount}`);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,10 @@
+const productName: string = "ノートパソコン";
+const price: number = 55000;
+const isOnSale: boolean = false;
+
+console.log(`商品名：${productName}`);
+console.log(`価格：${price}`);
+console.log(`セール中：${isOnSale}`);
+
+let stockCount = 2;
+console.log(`在庫数：${stockCount}`)
