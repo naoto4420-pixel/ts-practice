0d2ed7eab3f301a40a7536125102e81a03b72c88
naoto4420-pixel <naoto4420@gmail.com>
2026-02-26 13:47:52 +0900

    練習４（アロー関数、void）

--- index.js
+++ index.js
@@ -11,4 +11,29 @@ const userB = {
 };
 console.log(userA);
 console.log(userB);
+const calculateTotal = (price, amount) => {
+    return price * amount;
+};
+const total = calculateTotal(1500, 3);
+console.log(`合計金額は${total}円です`);
+const reportTestStatus = (productName, hasBugs) => {
+    if (hasBugs) {
+        console.log(`[報告] ${productName}は引き続き不具合の調査が必要です。`);
+    }
+    else {
+        console.log(`[報告] ${productName}はテスト完了、リリース可能です。`);
+    }
+};
+reportTestStatus("新作アクションゲーム", true);
+reportTestStatus("ECサイトのカート機能", false);
+const displayUserInfo = (user) => {
+    if (user.age) {
+        console.log(`ユーザー名: ${user.username}, 年齢: ${user.age}歳 (ID:${user.id})`);
+    }
+    else {
+        console.log(`ユーザー名: ${user.username}(ID:${user.id})`);
+    }
+};
+displayUserInfo(userA);
+displayUserInfo(userB);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -17,3 +17,32 @@ const userB: User = {
 
 console.log(userA);
 console.log(userB);
+
+const calculateTotal = (price: number, amount: number): number => {
+  return price * amount;
+};
+
+const total = calculateTotal(1500, 3);
+console.log(`合計金額は${total}円です`);
+
+const reportTestStatus = (productName: string, hasBugs: boolean): void => {
+  if (hasBugs){
+    console.log(`[報告] ${productName}は引き続き不具合の調査が必要です。`);
+  }else{
+    console.log(`[報告] ${productName}はテスト完了、リリース可能です。`);
+  }
+};
+
+reportTestStatus("新作アクションゲーム", true);
+reportTestStatus("ECサイトのカート機能", false);
+
+const displayUserInfo = (user: User): void => { 
+  if (user.age){
+    console.log(`ユーザー名: ${user.username}, 年齢: ${user.age}歳 (ID:${user.id})`);
+  }else{
+    console.log(`ユーザー名: ${user.username}(ID:${user.id})`);
+  }
+};
+
+displayUserInfo(userA);
+displayUserInfo(userB);
